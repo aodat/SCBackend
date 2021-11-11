61d6dd58b68c2faaa2817459d27f8c810d007af1
Abderhman123 <93913448+Abderhman123@users.noreply.github.com>
2021-11-11 13:05:17 +0200

    transaction date

--- app/Http/Controllers/API/Merchant/TransactionsController.php
+++ app/Http/Controllers/API/Merchant/TransactionsController.php
@@ -6,13 +6,14 @@ use App\Http\Requests\Merchant\TransactionRequest;
 
 use App\Models\Transaction;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class TransactionsController extends MerchantController
 {
     public function index(TransactionRequest $request)
     {
         $filters = $request->json()->all();
- 
+
         $since = $filters['created_at']['since'] ?? Carbon::today()->subDays(3)->format('Y-m-d');;
         $until = $filters['created_at']['until'] ?? Carbon::today()->format('Y-m-d');
 
@@ -21,52 +22,60 @@ class TransactionsController extends MerchantController
         $sources = $filters['sources'] ?? [];
         $amount = $filters['amount']['val'] ?? null;
         $operation = $filters['amount']['operation'] ?? null;
-        
-        $transaction = Transaction::whereBetween('created_at',[$since." 00:00:00",$until." 23:59:59"])
-                        ->where('merchant_id',$request->user()->merchant_id);
+        $transaction2 = Transaction::select('created_at')->groupBy('created_at')->paginate(request()->per_page ?? 10);
+        $paginated = array();
 
-        if(count($statuses))
-            $transaction->whereIn('status',$statuses);
+        foreach ($transaction2 as $key => $value) {
+            $transaction = Transaction::where("created_at", "=", $value->created_at)->whereBetween('created_at', [$since . " 00:00:00", $until . " 23:59:59"])
+                ->where('merchant_id', $request->user()->merchant_id);
 
-        if(count($sources))
-            $transaction->whereIn('source',$sources);
-                        
-        if(count($types))
-            $transaction->whereIn('type',$types);
-        
-        if($operation)
-            $transaction->where('amount',$operation, $amount);
-        else if($amount)
-            $transaction->whereBetween('amount', [intval($amount), intval($amount).'.99']);
-        $paginated = $transaction->paginate(request()->per_page ?? 10);
-    
-        return $this->response($paginated,'Data Retrieved Successfully',200,true);
+
+            if (count($statuses))
+                $transaction->whereIn('status', $statuses);
+
+            if (count($sources))
+                $transaction->whereIn('source', $sources);
+
+            if (count($types))
+                $transaction->whereIn('type', $types);
+
+            if ($operation)
+                $transaction->where('amount', $operation, $amount);
+            else if ($amount)
+                $transaction->whereBetween('amount', [intval($amount), intval($amount) . '.99']);
+
+            $created_at = (string)$value->created_at->format('Y-m-d');
+            $paginated[$created_at] = $transaction->get();
+        }
+
+
+        return $this->response($paginated, 'Data Retrieved Successfully', 200, false);
     }
 
-    public function show($id,TransactionRequest $request)
+    public function show($id, TransactionRequest $request)
     {
         $data = Transaction::findOrFail($id);
-        return $this->response($data,'Transaction Retrived Sucessfully',200);
+        return $this->response($data, 'Transaction Retrived Sucessfully', 200);
     }
-    
+
     public function withDraw(TransactionRequest $request)
     {
         $merchecntInfo = $this->getMerchentInfo();
-        
+
         $actualBalance = $merchecntInfo->actual_balance;
         $paymentMethod = $merchecntInfo->payment_methods;
         $paymentMethodID = $request->payment_method_id;
 
-        if($actualBalance < $request->amount)
-            return $this->response([],'The Actual Balance Not Enough',500);
+        if ($actualBalance < $request->amount)
+            return $this->response([], 'The Actual Balance Not Enough', 500);
 
 
         $merchecntInfo->actual_balance = $actualBalance - $request->amount;
         $merchecntInfo->save();
 
 
-        $selectedPayment = collect($paymentMethod)->reject(function ($value) use($paymentMethodID) {
-            if($value['id'] != $paymentMethodID)
+        $selectedPayment = collect($paymentMethod)->reject(function ($value) use ($paymentMethodID) {
+            if ($value['id'] != $paymentMethodID)
                 return $value;
         });
         $selectedPayment = array_values($selectedPayment->toArray());
@@ -81,12 +90,9 @@ class TransactionsController extends MerchantController
                 'payment_method' => collect($selectedPayment)
             ]
         );
-
-        return $this->successful();
     }
 
     public function export(TransactionRequest $request)
     {
-
     }
 }
