e8b6a6faaa5803a3231e32b3f61cb9baef4d68d1
Tareq Fawakhiri <tareq.fw@shipcash.net>
2021-11-30 16:43:23 +0200

    Fix Column error

---
+++ database/migrations/2021_11_30_144225_add_phone_verfived__at_to_merchants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPhoneVerfivedAtToMerchantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchants', function (Blueprint $table) {
+            $table->timestamp('phone_verified_at')->nullable()->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('merchants', function (Blueprint $table) {
+            //
+        });
+    }
+}
