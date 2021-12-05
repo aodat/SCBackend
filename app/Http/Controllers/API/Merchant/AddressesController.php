7fffede00e4dbe7a1c1266537622b9afc2696f10
Tareq Fawakhiri <tareq.fw@shipcash.net>
2021-12-05 16:04:54 +0200

    Update Addresses

--- app/Http/Controllers/API/Merchant/AddressesController.php
+++ app/Http/Controllers/API/Merchant/AddressesController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API\Merchant;
 
 use App\Exceptions\InternalException;
 use App\Http\Requests\Merchant\AddressesRequest;
-
+use App\Models\Area;
+use App\Models\City;
+use App\Models\Country;
 use App\Models\Merchant;
 use Carbon\Carbon;
 
@@ -19,21 +21,37 @@ class AddressesController extends MerchantController
 
     public function store(AddressesRequest $request)
     {
-        $merchantID = $request->user()->merchant_id;
-        $json = $request->json()->all();
+        $json = $request->validated();
+        $merchant = Merchant::where('id', $request->user()->merchant_id);
 
-        $merchant = Merchant::where('id', $merchantID);
+        $county_id = $request->county_id;
+        $city_id = $request->city_id;
+        $area_id = $request->area_id;
+
+        $country = Country::find($county_id);
+        $city = City::find($city_id);
+        $area = Area::find($area_id);
+
 
         $result = collect($merchant->select('addresses')->first()->addresses);
-        
-        if ($result->contains("name", $request->name))
-            throw new InternalException('name already Exists',400);
-
         $counter = $result->max('id') ?? 0;
-        $json['id'] = ++$counter;
-        $json['country'] = $request->country ?? 'JO';
-        $json['is_default'] = $request->is_default ?? false;
-        $json['created_at'] = Carbon::now();
+
+        if ($result->contains("name", $request->name))
+            throw new InternalException('name already Exists', 400);
+
+        $json = [
+            'id' => ++$counter,
+            'country_code' => $country->code,
+            'country' => $country->name_en,
+            'city_code' => $city->code,
+            'city' => $city->name_en,
+            'area' => $area->name_en,
+            'phone' => $request->phone,
+            'description' => $request->phone,
+            'is_default' => $request->is_default,
+            'created_at' => Carbon::now()
+        ];
+
         $merchant->update(['addresses' => $result->merge([$json])]);
         return $this->successful('Create Successfully');
     }
--- app/Http/Requests/Merchant/AddressesRequest.php
+++ app/Http/Requests/Merchant/AddressesRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests\Merchant;
 
-use App\Rules\Country;
-use App\Rules\City;
-use App\Rules\CountryCode;
+use Illuminate\Support\Facades\Request;
 
 class AddressesRequest extends MerchantRequest
 {
@@ -19,11 +17,9 @@ class AddressesRequest extends MerchantRequest
         if (strpos($path, 'addresses/create') !== false)
             return [
                 "name" => "required",
-                "country" => ["required"],
-                "country_code" => ["required", new CountryCode()],
-                "city" => ["required"],
-                "city_code" => "required",
-                "area" => "required",
+                "county_id" => "required|exists:countries,id",
+                "city_id" => "required|exists:cities,id,country_id,".Request::instance()->county_id,
+                "area_id" => "required|exists:areas,id,city_id,".Request::instance()->city_id,
                 "phone" => "required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10|max:14",
                 "description" => "required"
             ];
