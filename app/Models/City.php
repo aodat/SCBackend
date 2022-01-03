b2c960c4fbe3bb182ab65b8481d06204f6b94165
Tareq Fawakhiri <tareq.fw@shipcash.net>
2022-01-03 10:33:01 +0200

    return all cities

--- app/Models/City.php
+++ app/Models/City.php
@@ -13,18 +13,19 @@ class City extends Model
 
     public static function getAreas($id)
     {
+        $defult = collect([[
+            'id' => -1,
+            'city_id' => $id,
+            'name_ar' => 'جميع المناطق',
+            'name_en' => 'ALL',
+        ]]);
+
         $data = DB::table('cities as c')->join('areas as ar', 'c.id', 'ar.city_id')
             ->where('c.id', $id)
             ->orWhere('c.code', strtoupper($id))
             ->select('ar.*')
             ->get();
-        if ($data->isEmpty())
-            return [[
-                'id' => -1,
-                'city_id' => $id,
-                'name_ar' => 'جميع المناطق',
-                'name_en' => 'ALL',
-            ]];
-        return $data;
+
+        return $defult->merge($data);
     }
 }
