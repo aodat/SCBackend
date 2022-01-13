fd24deddf524f0b5bfcd5ef72f5addc08dfc9c07
Tareq Fawakhiri <tareq.fw@shipcash.net>
2022-01-13 11:32:25 +0200

    Finish Shipment

--- app/Http/Controllers/API/Merchant/DashboardController.php
+++ app/Http/Controllers/API/Merchant/DashboardController.php
@@ -10,33 +10,56 @@ use Carbon\CarbonPeriod;
 
 class DashboardController extends MerchantController
 {
-    private $since_at, $until, $merchant_id, $arrayDays;
     private $shippingCounter = [
         'DRAFT' => 0,
         'PROCESSING' => 0,
         'COMPLETED' => 0,
-        'RENTURND' => 0,
-        'PENDING_PAYMENTS' => 0,
+        'RENTURND' => 0
+    ];
+
+    private $paymentCounter = [
+        "CASHOUT" => 0,
+        "CASHIN" => 0,
+        'PENDING_PAYMENTS' => 0
     ];
 
     public function index(DashboardRequest $request)
     {
+        $merchant_id = $request->user()->merchant_id;
         $shipments = DB::table(DB::raw('shipments s'))
             ->select(DB::raw('date(updated_at) as date'), 'status', DB::raw('count(id) counter'))
-            ->where('s.merchant_id', '=', $request->user()->merchant_id)
+            ->where('s.merchant_id', '=', $merchant_id)
             ->groupByRaw('date(updated_at), status')
             ->get();
 
-        $dates = $shipping = [];
+        $transactions = DB::table(DB::raw('transactions t'))
+            ->select(DB::raw('date(updated_at) as date'), 'type as stype', DB::raw('count(id) counter'))
+            ->where([['t.merchant_id', $merchant_id], ['t.status', 'COMPLETED']])
+            ->groupByRaw('date(updated_at), type')
+            ->union(DB::table(DB::raw('shipments s'))
+                ->select(DB::raw('date(updated_at) as date'), DB::raw('"PENDING_PAYMENTS" as stype'), DB::raw('count(id) counter'))
+                ->where('s.merchant_id', '=', 11)
+                ->where('status', '=', 'COMPLETED')
+                ->whereNull('transaction_id')
+                ->groupByRaw('date(updated_at), status'))
+            ->get();
+
+
+        $shipping_dates = $payment_dates = $shipping = $payments = [];
 
         $period = CarbonPeriod::create($request->since_at, $request->until);
         foreach ($period as $date) {
             $current = $date->format('Y-m-d');
-            $dates[$current] = [
+            $shipping_dates[$current] = [
                 'DRAFT' => 0,
                 'PROCESSING' => 0,
                 'COMPLETED' => 0,
-                'RENTURND' => 0,
+                'RENTURND' => 0
+            ];
+
+            $payment_dates[$current] = [
+                'CASHIN' => 0,
+                'CASHOUT' => 0,
                 'PENDING_PAYMENTS' => 0
             ];
 
@@ -44,100 +67,44 @@ class DashboardController extends MerchantController
             $shipping['PROCESSING'][$current] = 0;
             $shipping['COMPLETED'][$current] = 0;
             $shipping['RENTURND'][$current] = 0;
-            $shipping['PENDING_PAYMENTS'][$current] = 0;
+
+            $payments['CASHIN'][$current] = 0;
+            $payments['CASHOUT'][$current] = 0;
+            $payments['PENDING_PAYMENTS'][$current] = 0;
         }
 
-        $shipments->map(function ($shipment) use (&$dates, &$shipping) {
+        $shipments->map(function ($shipment) use (&$shipping_dates, &$shipping) {
             $counter = $shipment->counter;
             $date = $shipment->date;
-
             $status = $shipment->status;
-            $dates[$date][$status] = $counter;
+
+            $shipping_dates[$date][$status] = $counter;
             $shipping[$status][$date] = $counter;
             $this->shippingCounter[$status] += $counter;
         });
 
-        $this->merchant_id =  $request->user()->merchant_id;
-        $this->until = $request->until;
-        $this->arrayDays = $this->arrayDays();
 
+        $transactions->map(function ($transaction) use (&$payment_dates, &$payments) {
+            $counter = $transaction->counter;
+            $date = $transaction->date;
+            $status = $transaction->stype;
 
-        $payment = $this->payment();
-        $pending_payment = $this->pending_payment();
+            $payment_dates[$date][$status] = $counter;
+            $payments[$status][$date] = $counter;
+            $this->paymentCounter[$status] += $counter;
+        });
+
         $data = [
             "chart" => [
                 "shipping" => $shipping,
-                "payment" => [
-                    "outcome" => $payment['CASHOUT'] ?? 0,
-                    "income" => $payment['CASHIN'] ?? 0,
-                    "pending_payment" => $pending_payment,
-                ]
+                "payment" => $payments
             ],
             "info" => [
                 "shipping" => $this->shippingCounter,
-                "payment" => [
-                    "outcome" =>  collect($payment['CASHOUT'] ?? 0)->sum(),
-                    "income" =>  collect($payment['CASHIN'] ?? 0)->sum(),
-                    "pending_payment" => $pending_payment->sum()
-                ]
+                "payment" =>  $this->paymentCounter
             ]
         ];
 
         return $this->response($data, 'Data Retrieved Successfully');
-    }
-
-    protected function arrayDays()
-    {
-        $start = new Carbon($this->since_at);
-        $blackoutDays[] =  $start->addDays(-1);
-        $end = new Carbon($this->until);
-        $days = $start->diff($end)->days;
-
-        $blackoutDays = array();
-        for ($i = 1; $i <= $days; $i++) {
-            $date = $start->addDays();
-            $blackoutDays[$date->format('Y-m-d')] = 0;
-        }
-
-        return collect($blackoutDays)->toArray();
-    }
-
-    protected function payment()
-    {
-        $payment = [
-            "CASHOUT" => $this->arrayDays,
-            "CASHIN" => $this->arrayDays,
-        ];
-        $payment_sql = DB::table('transactions')
-            ->whereBetween('created_at', [$this->since_at, $this->until])
-            ->where('merchant_id', '=',  $this->merchant_id)
-            ->select(DB::raw("DATE_FORMAT(created_at,'%Y-%m-%d') as date"), 'type', DB::raw('sum(amount) as amount'))
-            ->groupBy('date', 'type')
-            ->get();
-
-        $paymentCollect = collect($payment_sql);
-        foreach ($paymentCollect as  $value)
-            $payment[$value->type][$value->date] = $value->amount;
-
-        return $payment;
-    }
-
-    protected function pending_payment()
-    {
-        $pending_payment  = $this->arrayDays;
-        $pendingPaymentSql =   DB::table('transactions as t')
-            ->join('shipments as shp', 'shp.id', 't.item_id')
-            ->where('t.merchant_id', '=',  'shp.merchant_id')
-            ->where('shp.merchant_id', '=',  $this->merchant_id)
-            ->where('shp.transaction_id', '=',  null)
-            ->whereBetween('shp.created_at', [$this->since_at, $this->until])
-            ->select(DB::raw("DATE_FORMAT(shp.created_at,'%Y-%m-%d') as date"), DB::raw('sum(amount) as amount'))
-            ->groupBy('date')
-            ->get();
-        $pendingPaymentgCollect  = collect($pendingPaymentSql)->pluck('amount', 'date');
-        foreach ($pendingPaymentgCollect as  $key => $value)
-            $pending_payment[$key] = $value;
-
-        return collect($pending_payment);
     }
 }
