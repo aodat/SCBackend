9020789a90c31bf8aaeeea4233e9adca7fae274e
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-10 20:02:26 +0200

    .

--- app/Http/Requests/Merchant/DashboardRequest.php
+++ app/Http/Requests/Merchant/DashboardRequest.php
@@ -12,8 +12,8 @@ class DashboardRequest extends MerchantRequest
         $data = parent::all($keys);
         if ($this->method() == 'POST' && strpos($path, 'merchant/dashboard') !== false) {
             if ((isset($data['since_at']) && isset($data['until'])) && ($data['since_at'] !== null && $data['until'] !== null)) {
-                $data['since_at'] = date("Y-m-d H:i:s", strtotime($data['since_at']));
-                $data['until'] = date("Y-m-d H:i:s", strtotime($data['until']));
+                $data['since_at'] = Carbon::parse($data['since_at'])->startOfDay()->format('Y-m-d H:i:s');
+                $data['until'] = Carbon::parse($data['until'])->endOfDay()->format('Y-m-d H:i:s');
             } else {
                 $data['since_at'] = Carbon::now()->subYear(1);
                 $data['until'] = Carbon::tomorrow();
