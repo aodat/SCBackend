6a85e2e0fb566b78694b35343afa2d8e7fc23505
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-10 19:49:35 +0200

    .

--- app/Http/Requests/Merchant/DashboardRequest.php
+++ app/Http/Requests/Merchant/DashboardRequest.php
@@ -11,13 +11,12 @@ class DashboardRequest extends MerchantRequest
         $path = Request()->route()->uri;
         $data = parent::all($keys);
         if ($this->method() == 'POST' && strpos($path, 'merchant/dashboard') !== false) {
-
             if ((isset($data['since_at']) && isset($data['until'])) && ($data['since_at'] !== null && $data['until'] !== null)) {
                 $data['since_at'] = date("Y-m-d H:i:s", strtotime($data['since_at']));
                 $data['until'] = date("Y-m-d H:i:s", strtotime($data['until']));
             } else {
-                $data['since_at'] = Carbon::now()->subDays(14);
-                $data['until'] = Carbon::now();
+                $data['since_at'] = Carbon::now()->subYear(1);
+                $data['until'] = Carbon::tomorrow();
             }
         }
         return $data;
@@ -34,7 +33,7 @@ class DashboardRequest extends MerchantRequest
         if (strpos($path, 'merchant/dashboard') !== false) {
             return [
                 'since_at' => 'date',
-                'until' => 'date|after:since_at'
+                'until' => 'date|after:since_at',
             ];
         }
         return [];
