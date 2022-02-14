bd30cbe237a4750d50ef4eeab402f4ac17515c92
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-14 09:52:59 +0200

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Utilities\Shipcash;
-use App\Models\Merchant;
-use App\Models\Shipment;
-use App\Models\User;
 use App\Traits\CarriersManager;
 use App\Traits\ResponseHandler;
 use App\Traits\SystemRules;
-use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
