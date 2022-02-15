c30e41dba43c72a97d9f5cac9d30a8aeed7d25f3
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-15 10:11:25 +0200

    withdraw using dinark

--- app/Http/Controllers/API/Merchant/TransactionsController.php
+++ app/Http/Controllers/API/Merchant/TransactionsController.php
@@ -103,7 +103,7 @@ class TransactionsController extends MerchantController
             return $this->error('You dont have COD Balance');
         }
 
-        // $dinarak->withdraw($merchecntInfo, $payment['iban'], $dedaction);
+        $result = $dinarak->withdraw($merchecntInfo, $payment['iban'], $dedaction);
 
         $merchecntInfo->cod_balance -= $merchecntInfo->amount;
         $merchecntInfo->save();
@@ -115,6 +115,7 @@ class TransactionsController extends MerchantController
             "created_by" => Request()->user()->id,
             "merchant_id" => Request()->user()->merchant_id,
             'amount' => $dedaction,
+            'notes' => json_encode($result),
             'status' => 'PROCESSING',
             "balance_after" => $merchecntInfo->cod_balance - $dedaction,
             "source" => "NONE",
