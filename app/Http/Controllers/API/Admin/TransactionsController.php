1c43e00e25561707b083dc98b6bb8a5753de23b6
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-17 13:53:05 +0200

    remove service fees

--- app/Http/Controllers/API/Admin/TransactionsController.php
+++ app/Http/Controllers/API/Admin/TransactionsController.php
@@ -41,7 +41,7 @@ class TransactionsController extends Controller
             return $this->error('This transaction was paid');
         }
 
-        $dedaction = $transaction->amount;
+        $dedaction = $transaction->amount - 0.35;
 
         // This Tranasction for testing only
         $result = '{"id":898353,"name":"Tareq Fawakhiri","receiverID":"962772170353","amount":1,"description":"Transfer from ShipCash","creationDate":"2022-02-17T08:52:13.6572354Z","approvalDate":"2022-02-17T08:52:13.6831651Z","reference":"83549122033917620e0c349480d422354571","financialReference":"266695128792580","status":{"id":1,"name":"Success","errorMessage":null}}';
