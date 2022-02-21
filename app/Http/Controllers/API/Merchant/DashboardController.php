1d2c5584f2be464c9c5c1cf744e315b63902e5df
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-21 15:29:06 +0200

    revamp dashboard

--- app/Http/Controllers/API/Merchant/DashboardController.php
+++ app/Http/Controllers/API/Merchant/DashboardController.php
@@ -3,116 +3,136 @@
 namespace App\Http\Controllers\API\Merchant;
 
 use App\Http\Requests\Merchant\DashboardRequest;
-use Carbon\CarbonPeriod;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends MerchantController
 {
-    private $shippingCounter = [
+    private $shippinInfoCard = [
+        'DRAFT' => ['counter' => 0, 'rate' => 0],
+        'PROCESSING' => ['counter' => 0, 'rate' => 0],
+        'COMPLETED' => ['counter' => 0, 'rate' => 0],
+        'RENTURND' => ['counter' => 0, 'rate' => 0],
+    ];
+
+    private $overall = [
         'DRAFT' => 0,
         'PROCESSING' => 0,
         'COMPLETED' => 0,
         'RENTURND' => 0,
-    ];
-
-    private $paymentCounter = [
         "CASHOUT" => 0,
         "CASHIN" => 0,
         'PENDING_PAYMENTS' => 0,
     ];
 
+    private $shippingChart = [
+        'DRAFT' => null,
+        'PROCESSING' => null,
+        'COMPLETED' => null,
+        'RENTURND' => null,
+    ];
+
+    private $paymentChart = [
+        'CASHIN' => null,
+        'CASHOUT' => null,
+        'PENDING_PAYMENTS' => null,
+    ];
+
+    private $paymentInfoCard = [
+        'CASHIN' => ['counter' => 0, 'amount' => 0],
+        'CASHOUT' => ['counter' => 0, 'amount' => 0],
+        'PENDING_PAYMENTS' => ['counter' => 0, 'amount' => 0],
+    ];
+
     public function index(DashboardRequest $request)
     {
         $merchant_id = $request->user()->merchant_id;
+
         $shipments = DB::table(DB::raw('shipments s'))
-            ->select(DB::raw('date(updated_at) as date'), 'status', DB::raw('count(id) counter'))
             ->where('s.merchant_id', '=', $merchant_id)
-            ->where('s.is_deleted', false)
+            ->where('s.is_deleted', false);
+
+        $shippingOverAll = $shipments->select('status', DB::raw('count(id) counter'))
+            ->groupByRaw('status')
+            ->pluck('counter', 'status')
+            ->toArray();
+
+        $shipping = $shipments->select(DB::raw('date(updated_at) as date'), 'status', DB::raw('count(id) counter'))
             ->whereBetween('s.updated_at', [$request->since_at, $request->until])
             ->groupByRaw('date(updated_at), status')
             ->get();
 
+        $totalData = $shipping->sum('counter');
+        $shipping->map(function ($shipment) {
+            $status = $shipment->status;
+            $date = $shipment->date;
+
+            $this->shippingChart[$status][$date] = $shipment->counter;
+            $this->shippinInfoCard[$status]['counter'] += $shipment->counter;
+        });
+
+        if ($totalData > 0) {
+            $this->shippinInfoCard = array_map(function ($data) use ($totalData) {
+                return [
+                    'counter' => $data['counter'],
+                    'rate' => ($data['counter'] > 0 ? round(($data['counter'] / $totalData) * 100) : 0) . '%',
+                ];
+
+            }, $this->shippinInfoCard);
+        }
+
         $transactions = DB::table(DB::raw('transactions t'))
             ->select(DB::raw('date(updated_at) as date'), 'type as stype', DB::raw('count(id) counter'), DB::raw('sum(amount) as total'))
             ->where([['t.merchant_id', $merchant_id], ['t.status', 'COMPLETED']])
-            ->whereBetween('t.updated_at', [$request->since_at, $request->until])
             ->where('t.subtype', 'COD')
-            ->groupByRaw('date(updated_at), type')
-            ->union(DB::table(DB::raw('shipments s'))
-                    ->select(
-                        DB::raw('date(updated_at) as date'),
-                        DB::raw('"PENDING_PAYMENTS" as stype'),
-                        DB::raw('count(id) counter'),
-                        DB::raw('sum(cod) as total')
-                    )
-                    ->where('s.merchant_id', '=', $merchant_id)
-                    ->where('status', '=', 'COMPLETED')
-                    ->where('s.is_deleted', false)
-                    ->whereBetween('s.updated_at', [$request->since_at, $request->until])
-                    ->whereNull('transaction_id')
-                    ->groupByRaw('date(updated_at), status'))
-            ->get();
+            ->groupByRaw('date(updated_at), type');
 
-        $shipping_dates = $payment_dates = $shipping = $payments = [];
+        $pendingPayments = DB::table(DB::raw('shipments s'))
+            ->select(
+                DB::raw('date(updated_at) as date'),
+                DB::raw('"PENDING_PAYMENTS" as stype'),
+                DB::raw('count(id) counter'),
+                DB::raw('sum(cod) as total')
+            )
+            ->where('s.merchant_id', '=', $merchant_id)
+            ->where('status', '=', 'COMPLETED')
+            ->where('s.is_deleted', false)
+            ->whereNull('transaction_id')
+            ->groupByRaw('date(updated_at), status');
 
-        $period = CarbonPeriod::create($request->since_at, $request->until);
-        foreach ($period as $date) {
-            $current = $date->format('Y-m-d');
-            $shipping_dates[$current] = [
-                'DRAFT' => 0,
-                'PROCESSING' => 0,
-                'COMPLETED' => 0,
-                'RENTURND' => 0,
-            ];
+        $transactionOverAll = $transactions->union(
+            $pendingPayments
+        );
 
-            $payment_dates[$current] = [
-                'CASHIN' => 0,
-                'CASHOUT' => 0,
-                'PENDING_PAYMENTS' => 0,
-            ];
+        $transactionByDates = $transactions->union(
+            $pendingPayments
+        )->get()->whereBetween('date', [$request->since_at, $request->until]);
 
-            $shipping['DRAFT'][$current] = 0;
-            $shipping['PROCESSING'][$current] = 0;
-            $shipping['COMPLETED'][$current] = 0;
-            $shipping['RENTURND'][$current] = 0;
+        $this->overall = array_merge($this->overall, $shippingOverAll, $transactionOverAll->pluck('total', 'stype')->toArray());
 
-            $payments['CASHIN'][$current] = 0;
-            $payments['CASHOUT'][$current] = 0;
-            $payments['PENDING_PAYMENTS'][$current] = 0;
-        }
-
-        $shipments->map(function ($shipment) use (&$shipping_dates, &$shipping) {
-            $counter = $shipment->counter;
-            $date = $shipment->date;
-            $status = $shipment->status;
-
-            $shipping_dates[$date][$status] = $counter;
-            $shipping[$status][$date] = $counter;
-            $this->shippingCounter[$status] += $counter;
-        });
-
-        $transactions->map(function ($transaction) use (&$payment_dates, &$payments) {
-            $total = $transaction->total;
-            $counter = $transaction->counter;
+        $transactionByDates->map(function ($transaction) {
+            $type = $transaction->stype;
             $date = $transaction->date;
-            $status = $transaction->stype;
+            $counter = $transaction->counter;
+            $total = $transaction->total;
 
-            $payment_dates[$date][$status] = $counter;
-            $payments[$status][$date] = $counter;
-            $this->paymentCounter[$status] += $total;
+            $this->paymentChart[$type][$date] = $counter;
+            $this->paymentInfoCard[$type] = [
+                'counter' => $counter,
+                'amount' => $total,
+            ];
         });
 
         $data = [
-            "chart" => [
-                "shipping" => $shipping,
-                "payment" => $payments,
+            'overall' => $this->overall,
+            'shipping' => [
+                'cards' => $this->shippinInfoCard,
+                'chart' => $this->shippingChart,
             ],
-            "info" => [
-                "shipping" => $this->shippingCounter,
-                "payment" => $this->paymentCounter,
+            'payments' => [
+                'cards' => $this->paymentInfoCard,
+                'chart' => $this->paymentChart,
             ],
         ];
-
         return $this->response($data, 'Data Retrieved Successfully');
     }
 }
--- app/Http/Requests/Merchant/DashboardRequest.php
+++ app/Http/Requests/Merchant/DashboardRequest.php
@@ -12,8 +12,8 @@ class DashboardRequest extends MerchantRequest
         $data = parent::all($keys);
         if ($this->method() == 'POST' && strpos($path, 'merchant/dashboard') !== false) {
             if ((isset($data['since_at']) && isset($data['until'])) && ($data['since_at'] !== null && $data['until'] !== null)) {
-                $data['since_at'] = Carbon::parse($data['since_at'])->startOfDay()->format('Y-m-d H:i:s');
-                $data['until'] = Carbon::parse($data['until'])->endOfDay()->format('Y-m-d H:i:s');
+                $data['since_at'] = Carbon::parse($data['since_at'])->format('Y-m-d');
+                $data['until'] = Carbon::parse($data['until'])->format('Y-m-d');
             } else {
                 $data['since_at'] = Carbon::now()->subYear(1);
                 $data['until'] = Carbon::tomorrow();
@@ -33,7 +33,7 @@ class DashboardRequest extends MerchantRequest
         if (strpos($path, 'merchant/dashboard') !== false) {
             return [
                 'since_at' => 'date',
-                'until' => 'date|after:since_at',
+                'until' => 'date|after_or_equal:since_at',
             ];
         }
         return [];
