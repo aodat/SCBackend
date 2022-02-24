dddd9f59cd1227289e2498cc74f43d056912c360
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-02-24 10:52:53 +0200

    Fix update dom in admin

--- app/Http/Controllers/API/Admin/DomesticRatesController.php
+++ app/Http/Controllers/API/Admin/DomesticRatesController.php
@@ -23,21 +23,23 @@ class DomesticRatesController extends Controller
     public function update(DomesticRatesRequest $request)
     {
         $carrier_id = $request->carrier_id;
-        $merchant = $this->merchant;
+        $zone_id    = $request->id;
+        $price      = $request->price;
+
+        $merchant   = $this->merchant;
         $domestic_rates = $merchant->domestic_rates;
 
-        $rates = collect($domestic_rates[$carrier_id]);
-        $rate = $rates->where('id', $request->id);
 
-        if ($rate->first() == null) {
-            throw new InternalException('Rate id not Exists');
-        }
-
-        $current = $rate->keys()->first();
-
-        $domestic_rates[$carrier_id][$current]['price'] = $request->price;
-
-        $merchant->update(['domestic_rates' => collect($domestic_rates)]);
+        $new_dom_rates = collect($domestic_rates)->map(function ($data) use ($carrier_id, $zone_id, $price) {
+            if ($data['carrier_id'] == $carrier_id) {
+                foreach ($data['zones'] as $key => $value) {
+                    if ($value['id'] == $zone_id)
+                        $data['zones'][$key]['price'] = $price;
+                }
+            }
+            return $data;
+        });
+        $merchant->update(['domestic_rates' => $new_dom_rates]);
         return $this->successful('Updated Successfully');
     }
 }
