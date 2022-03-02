a32951490862bcd648870723a73b90b8506e63ba
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-03-02 14:59:22 +0300

    add guest controller

---
+++ app/Http/Requests/Merchant/GuestRequest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Requests\Merchant;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Validation\Rules\RequiredIf;
+
+class GuestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $path = Request()->route()->uri;
+        if ($this->method() == 'POST' && strpos($path, 'shipments/calculate/fees')) {
+            $type = Request::instance()->type;
+            $validation = [
+                'weight' => 'required|numeric|between:0,9999',
+                'type' => 'required|in:express,domestic',
+                'is_cod' => 'required|boolean',
+            ];
+            if ($type == 'express') {
+                $validation['country_code'] = 'required';
+                if (!empty(Request::instance()->dimention)) {
+                    $validation['dimention.length'] = 'required|numeric|between:0,9999';
+                    $validation['dimention.height'] = 'required|numeric|between:0,9999';
+                    $validation['dimention.width'] = 'required|numeric|between:0,9999';
+                }
+            } else {
+                $validation['city_from'] = 'required';
+                $validation['city_to'] = 'required';
+            }
+            return $validation;
+        } else if ($this->method() == 'POST' && (strpos($path, 'shipments/create') !== false)) {
+            dd('Error ');
+            $validation = [
+                'strip_token' => 'required',
+                'type' => 'required|in:express,domestic',
+                'carrier_id' => [
+                    'required',
+                    'exists:carriers,id,is_active,1',
+                ],
+                'sender_email' => 'required|email',
+                'sender_name' => [
+                    'required'
+                ],
+                'sender_phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10|max:14',
+                'sender_country' => 'required',
+                'sender_city' => 'required',
+                'sender_area' => 'required',
+                'sender_address_description' => 'required',
+
+                'consignee_name' => 'required|max:255',
+                'consignee_email' => 'email',
+                'consignee_phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+                'consignee_notes' => [
+                    // new wordCount(1, 10),
+                ],
+                'consignee_country' => 'required',
+                'consignee_city' => 'required',
+                'consignee_area' => 'required',
+                'consignee_address_description_1' => [
+                    'required',
+                ],
+                'consignee_address_description_2' => '',
+                'content' => [
+                    'required',
+                ],
+                'pieces' => 'required|integer',
+                'consignee_zip_code' => '',
+                'actual_weight' => 'required|numeric|between:0,9999',
+            ];
+
+            $type = Request::instance()->type;
+            if ($type == 'express') {
+                $validation['cod'] = 'numeric|between:0,9999';
+                $validation['payment'] = 'numeric|between:0,9999';
+                $validation['consignee_country'] = 'required';
+                $validation['actual_weight'] = 'required|numeric|between:0,9999';
+                $validation['is_doc'] = 'required|boolean';
+                $validation['declared_value'] = [
+                    new RequiredIf($this->is_doc == false),
+                    'numeric',
+                    'between:0,9999',
+                ];
+                $validation['consignee_zip_code'] = '';
+                $validation['consignee_second_phone'] = '';
+            } else {
+                $validation['cod'] = 'required|numeric|between:0,9999';
+                $validation['extra_services'] = 'required|in:DOMCOD';
+            }
+            return $validation;
+        }
+
+        return [];
+    }
+}
--- routes/guest.php
+++ routes/guest.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\Merchant\MerchantController;
-use App\Http\Controllers\API\Merchant\ShipmentController;
+use App\Http\Controllers\API\Merchant\GuestController;
 use App\Http\Controllers\Controller;
 
 Route::group(['middleware' => ['json.response']], function () {
@@ -10,7 +10,7 @@ Route::group(['middleware' => ['json.response']], function () {
     Route::get('cities/{city_code}', [MerchantController::class, 'getCities']);
     Route::get('areas/{area_code}', [MerchantController::class, 'getAreas']);
 
-    Route::post('shipments/calculate/fees', [ShipmentController::class, 'calculate']);
+    Route::post('shipments/calculate/fees', [GuestController::class, 'calculate']);
     // Route::post('shipments/create', [ShipmentController::class, 'guestShipment']);
 
 
