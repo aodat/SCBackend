a8e8aa619fc1a6b027f3c49de6b44b5f38b37540
Tareq Fawakhiri <92667919+Tareq-Fw@users.noreply.github.com>
2022-03-21 00:44:18 +0300

    .

--- app/Console/Commands/CODTransactions.php
+++ app/Console/Commands/CODTransactions.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\API\Merchant\TransactionsController;
 use App\Models\Merchant;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -21,7 +20,7 @@ class CODTransactions extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'SH239 COD Balance';
 
     /**
      * Create a new command instance.
@@ -38,46 +37,54 @@ class CODTransactions extends Command
      *
      * @return int
      */
-    public function handle(TransactionsController $transaction)
+    public function handle()
     {
-        $shipments = DB::table('shipments')
-            ->select('id', 'awb', 'merchant_id', 'cod', 'fees', 'created_by')
-            ->whereRaw('date(paid_at) = date(now())')
-            ->whereNull('transaction_id')
-            ->where('is_collected', true)
-            ->get();
+        DB::transaction(function () {
 
-        $shipments->map(function ($shipment) use ($transaction) {
-            $id = $shipment->id;
-            $merchant_id = $shipment->merchant_id;
+            $shipments = DB::table('shipments')
+                ->select('id', 'awb', 'merchant_id', 'cod', 'fees', 'created_by')
+                ->whereRaw('date(paid_at) = date(now())')
+                ->whereNull('transaction_id')
+                ->where('is_collected', true)
+                ->get();
 
-            $merchant = Merchant::findOrFail($merchant_id);
+            $merchant = 
+            $shipments->map(function ($shipment) {
+                $id = $shipment->id;
+                $merchant_id = $shipment->merchant_id;
+                $cod = $shipment->cod;
+                $fees = $shipment->fees;
+                $awb = $shipment->awb;
+                $created_by = $shipment->created_by;
 
-            $cod = $shipment->cod;
-            $fees = $shipment->fees;
-            $awb = $shipment->awb;
-            $created_by = $shipment->created_by;
+                $merchant = Merchant::findOrFail($merchant_id);
+                if ($merchant->payment_type == 'POSTPAID') {
+                    $amount = $cod - $fees;
+                } else {
+                    $amount = $cod;
+                }
 
+                $merchant->cod_balance += $amount;
+                $merchant->save();
 
-            if ($merchant->payment_type == 'POSTPAID') {
-                $amount = $cod - $fees;
-            } else {
-                $amount = $cod;
-            }
-
-            $transaction_id = $transaction->COD(
-                'CASHIN',
-                $merchant_id,
-                $awb,
-                $amount,
-                "SHIPMENT",
-                $created_by,
-                'Aramex SH239 webhook',
-                'COMPLETED',
-                'API'
-            );
-
-            DB::table('shipments')->where('id', $id)->update(['transaction_id' => $transaction_id]);
+                $transaction_id = DB::table('transactions')->insertGetId(
+                    [
+                        'type' => 'CASHIN',
+                        'subtype' => 'COD',
+                        'item_id' => $awb,
+                        'merchant_id' => $merchant_id,
+                        'description' => 'Aramex SH239 webhook',
+                        'balance_after' => $merchant->cod_balance,
+                        'amount' => $amount,
+                        'source' => 'SHIPMENT',
+                        'status' => 'COMPLETED',
+                        'created_by' => $created_by,
+                        'resource' => 'API',
+                        'payment_method' => null
+                    ]
+                );
+                DB::table('shipments')->where('id', $id)->update(['transaction_id' => $transaction_id]);
+            });
         });
         return Command::SUCCESS;
     }
